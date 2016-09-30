1e00489893b84ba13334a64232e8a7304d995595
Shanu Kumar <sshanukr@gmail.com>
2016-10-01 00:03:16 +0530

    Delete install.sh

--- install.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash 
-# download model.net
- wget "https://doc-10-4s-docs.googleusercontent.com/docs/securesc/2cmemufrfvetn6pi5tv5b31pkd9g1q9h/tnapt2eaa4al4or8vatu3dimac74om2v/1475258400000/08229438665543725920/08229438665543725920/0B8bmgDbwE_77ZDNBeW45bVdsT0U?e=download&nonce=se5fgo8jpa6pq&user=08229438665543725920&hash=1c8f8dlopc13nl4b0jr8v7a0s66aef94" -O model.net
-
-# done!
-echo "==> Install completed, execute 'run.lua'"
